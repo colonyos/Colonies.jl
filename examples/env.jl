2e9827f6f21c315f885c9cebdac580031be7e281
Johan Kristiansson <kristiansson.johan@gmail.com>
2024-07-19 15:05:55 +0200

    new example files

---
+++ examples/env.jl
@@ -0,0 +1,46 @@
+using Pkg
+
+Pkg.activate("..")
+
+using Colonies
+using Colonies.Crypto
+
+function main()
+	client, colonyname, colony_prvkey, executorname, prvkey = Colonies.client()
+	
+	conditions = Colonies.Conditions(colonyname=colonyname, 
+                                     executornames=String["dev-docker"],
+                                     executortype="container-executor",
+                                     walltime=60)
+
+    env = Dict{Any, Any}()
+	env["MYVAR"] = "myvalue"
+	
+	kwargs = Dict{Any, Any}()
+	kwargs["cmd"] = "env"
+	kwargs["docker-image"] = "ubuntu:20.04"
+
+	funcspec = Colonies.FunctionSpec(funcname="execute",
+	                                 kwargs=kwargs,
+	                                 maxretries=3,
+	                                 maxexectime=55,
+	                                 conditions=conditions,
+	                                 label="myprocess",
+	                                 fs=Colonies.Filesystem(),
+	                                 env=env)
+
+    process = Colonies.submit(client, funcspec, prvkey)
+	println("Process submitted: ", process.processid)
+
+	println("Waiting for process to finish ...")
+	Colonies.wait(client, process, 60, prvkey)
+	println("Process finished")
+
+	println("Getting 100 last logs")
+	logs = Colonies.getlogs(client, colonyname, process.processid, 100, 0, prvkey)
+	for log in logs
+		print(log.message)
+	end
+end
+
+main()
